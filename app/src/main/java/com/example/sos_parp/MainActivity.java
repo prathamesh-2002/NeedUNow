9655223e10c3b65a68ca3e216309ff300edeee75
PRATHMESHMANE <prathmeshmane334@gmail.com>
2021-05-13 20:47:16 +0530

    Helpline UI Added

--- app/src/main/java/com/example/sos_parp/MainActivity.java
+++ app/src/main/java/com/example/sos_parp/MainActivity.java
@@ -35,8 +35,9 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_about)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    //    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-  //      NavigationUI.setupWithNavController(navView, navController);
+       NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+
     }
 
 }
