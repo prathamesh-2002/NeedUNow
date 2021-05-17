cad7e264827ba679ba5ade4dbe3dc2709ac9fc89
Prathamesh Punde⚡ <propunde99@gmail.com>
2021-05-17 16:37:46 +0530

    Added custom toolbar and modified bottom nav bar

--- app/src/main/java/com/example/sos_parp/MainActivity.java
+++ app/src/main/java/com/example/sos_parp/MainActivity.java
@@ -8,12 +8,15 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
+
+//    private Toolbar toolbar = findViewById(R.id.toolbar);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,20 +26,21 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
 
-  /*      ActionBar actionBar;
+   /*     ActionBar actionBar;
         actionBar = getSupportActionBar();
-
+        actionBar.hide();
         ColorDrawable colorDrawable
                 = new ColorDrawable(Color.parseColor("#FFFFFF"));
 
         actionBar.setBackgroundDrawable(colorDrawable);
   */
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+   /*     AppBarConfiguration appBarConfiguration = ew AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_about)
-                .build();
+                .build();*/
+      //  setActionBar(Toolbar toolbar);
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    //    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-  //      NavigationUI.setupWithNavController(navView, navController);
+    //    NavigationUI.setupActionBarWithNavController(this, navController);
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
 }
