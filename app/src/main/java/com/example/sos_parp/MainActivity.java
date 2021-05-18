7c8e50aeb9a1f2edafee5f878a8eb303d3bea637
Prathamesh Punde⚡ <propunde99@gmail.com>
2021-05-18 18:20:28 +0530

    crash

--- app/src/main/java/com/example/sos_parp/MainActivity.java
+++ app/src/main/java/com/example/sos_parp/MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -15,28 +16,54 @@ import androidx.navigation.ui.NavigationUI;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
 
-  /*      ActionBar actionBar;
-        actionBar = getSupportActionBar();
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
 
-        ColorDrawable colorDrawable
-                = new ColorDrawable(Color.parseColor("#FFFFFF"));
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.outline_help_outline_24);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        actionBar.setBackgroundDrawable(colorDrawable);
-  */
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_about)
-                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-    //    NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-  //      NavigationUI.setupWithNavController(navView, navController);
+
+        NavigationUI.setupWithNavController(navView, navController);
     }
 
+
+
+
 }
+/*
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.toolbar_nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+
+            case R.id.navigation_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+
+            case android.R.id.home:
+                startActivity(new Intent(this, HelpActivity.class));
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+ */
